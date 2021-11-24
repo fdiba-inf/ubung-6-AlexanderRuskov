c243b5dd580b035d4c9d05614dea10a70f19fd8d
AlexanderRuskov <>
2021-11-24 16:12:15 +0000

    Update

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -24,7 +24,7 @@ public class PasswordCheck {
             break;
           }
           boolean digit = Character.isDigit(symbol);
-          if (digit) {git add .
+          if (digit) {
             digitCount++;
           }
         }
